88f2bbfd91f582264c4723253373692b5c62bf8f
leojomar01 <leojomar01@gmail.com>
2023-01-15 09:44:24 +0800

    add $ sign

--- computation.js
+++ computation.js
@@ -3,10 +3,10 @@ const AT = document.getElementById('AT');
 AT.addEventListener('change', (event) => {
 
   const c1r1 = document.querySelector('.c1r1');
-  c1r1.textContent = 100 + (4.5 * event.target.value);
+  c1r1.textContent = "$"+(100 + (4.5 * event.target.value));
 
   const c2r1 = document.querySelector('.c2r1');
-  c2r1.textContent = 500 + (40.5 * event.target.value * 2);
+  c2r1.textContent = "$"+(500 + (40.5 * event.target.value * 2));
 
   const c3r1 = document.querySelector('.c3r1');
   c3r1.textContent = 500 * (.5 * event.target.value * 2);
